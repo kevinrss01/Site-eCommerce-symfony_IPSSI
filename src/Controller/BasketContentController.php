25e326fafd766eea972e1253263d59ce58e08292
kevinrss01 <kevin.rousseau3@gmail.com>
2022-12-08 15:08:52 +0100

    crud pour panier

---
+++ src/Controller/BasketContentController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\BasketContent;
+use App\Form\BasketContentType;
+use App\Repository\BasketContentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/basket/content')]
+class BasketContentController extends AbstractController
+{
+    #[Route('/', name: 'app_basket_content_index', methods: ['GET'])]
+    public function index(BasketContentRepository $basketContentRepository): Response
+    {
+        return $this->render('basket_content/index.html.twig', [
+            'basket_contents' => $basketContentRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_basket_content_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, BasketContentRepository $basketContentRepository): Response
+    {
+        $basketContent = new BasketContent();
+        $form = $this->createForm(BasketContentType::class, $basketContent);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $basketContentRepository->save($basketContent, true);
+
+            return $this->redirectToRoute('app_basket_content_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('basket_content/new.html.twig', [
+            'basket_content' => $basketContent,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_basket_content_show', methods: ['GET'])]
+    public function show(BasketContent $basketContent): Response
+    {
+        return $this->render('basket_content/show.html.twig', [
+            'basket_content' => $basketContent,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_basket_content_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, BasketContent $basketContent, BasketContentRepository $basketContentRepository): Response
+    {
+        $form = $this->createForm(BasketContentType::class, $basketContent);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $basketContentRepository->save($basketContent, true);
+
+            return $this->redirectToRoute('app_basket_content_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('basket_content/edit.html.twig', [
+            'basket_content' => $basketContent,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_basket_content_delete', methods: ['POST'])]
+    public function delete(Request $request, BasketContent $basketContent, BasketContentRepository $basketContentRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$basketContent->getId(), $request->request->get('_token'))) {
+            $basketContentRepository->remove($basketContent, true);
+        }
+
+        return $this->redirectToRoute('app_basket_content_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/BasketType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Basket;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BasketType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('buyDate')
+            ->add('state')
+            ->add('owner')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Basket::class,
+        ]);
+    }
+}
