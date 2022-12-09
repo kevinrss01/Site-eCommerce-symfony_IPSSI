f8a078a23fd7082e16f2a288f6c36e2e2a6a206e
kevinrss01 <kevin.rousseau3@gmail.com>
2022-12-09 09:33:41 +0100

    contrainte admin + navbar

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -22,7 +22,7 @@ class ProductsController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_products_new', methods: ['GET', 'POST'])]
+    #[Route('/action/new', name: 'app_products_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ProductsRepository $productsRepository): Response
     {
         $product = new Products();
@@ -73,7 +73,7 @@ class ProductsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_products_edit', methods: ['GET', 'POST'])]
+    #[Route('/action/{id}/edit', name: 'app_products_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Products $product, ProductsRepository $productsRepository): Response
     {
         $form = $this->createForm(ProductsType::class, $product);
@@ -91,7 +91,7 @@ class ProductsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_products_delete', methods: ['POST'])]
+    #[Route('/action/{id}', name: 'app_products_delete', methods: ['POST'])]
     public function delete(Request $request, Products $product, ProductsRepository $productsRepository): Response
     {
         if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
