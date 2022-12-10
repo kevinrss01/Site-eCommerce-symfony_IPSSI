21da46345fde3d3d6f87c07710d6f4d90adf68f3
Lansor <killian28100@gmail.com>
2022-12-10 17:47:43 +0100

    validation panier

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -20,7 +20,7 @@ class BasketController extends AbstractController
     #[Route('/', name: 'app_basket_index', methods: ['GET'])]
     public function index(BasketRepository $basketRepository, BasketContentRepository $basketContentRepository): Response
     {
-        if(!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
@@ -33,10 +33,10 @@ class BasketController extends AbstractController
     #[Route('/allBaskets', name: 'app_all_basket_index', methods: ['GET'])]
     public function indexAll(BasketRepository $basketRepository): Response
     {
-        if(!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
-        
+
         return $this->render('basket/allBaskets.html.twig', [
             'baskets' => $basketRepository->findAll(),
         ]);
@@ -44,30 +44,31 @@ class BasketController extends AbstractController
 
 
 
-    #[Route('basket/action/new', name: 'app_basket_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, BasketRepository $basketRepository , Basket $basket = null): Response
+    #[Route('basket/action/new/{basket}', name: 'app_basket_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, BasketRepository $basketRepository, Basket $basket = null): Response
     {
-        if(!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
-
-        if($basket == null) {
-            return $this->redirectToRoute('app_basket_content_index');
+        $user = $this->getUser();
+        $userid = $user->getId();
+        if ($basket == null) {
+            return $this->redirectToRoute('app_basket_content_index', array('user' => $userid), Response::HTTP_SEE_OTHER);
         }
+         $basket =$basketRepository->findById($basket);
         $basket->setBuyDate(new \DateTime());
         $basket->setState(True);
         $basketRepository->save($basket, true);
-        $newBasket =new Basket();
+        $newBasket = new Basket();
         $newBasket->setOwner($basket->getOwner());
-        $basketRepository->save($newBasket,true);
-        return $this->redirectToRoute('app_basket_content_index', [], Response::HTTP_SEE_OTHER);
-
+        $basketRepository->save($newBasket, true);
+        return $this->redirectToRoute('app_basket_content_index', array('user' => $userid), Response::HTTP_SEE_OTHER);
     }
 
     #[Route('/basket/{id}', name: 'app_basket_show', methods: ['GET'])]
     public function show(Basket $basket): Response
     {
-        if(!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
@@ -78,7 +79,7 @@ class BasketController extends AbstractController
     #[Route('basket/action/{id}/edit', name: 'app_basket_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Basket $basket, BasketRepository $basketRepository): Response
     {
-        if(!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
@@ -100,11 +101,11 @@ class BasketController extends AbstractController
     #[Route('/basket/action/{id}', name: 'app_basket_delete', methods: ['POST'])]
     public function delete(Request $request, Basket $basket, BasketRepository $basketRepository): Response
     {
-        if(!$this->getUser()){
+        if (!$this->getUser()) {
             return $this->redirectToRoute('app_login');
         }
 
-        if ($this->isCsrfTokenValid('delete'.$basket->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $basket->getId(), $request->request->get('_token'))) {
             $basketRepository->remove($basket, true);
         }
 
